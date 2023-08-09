5b8a18d46fe92c4c479d78e1a741f4e9d0b183c7
Harshit Raj <93223125+iamrajharshit@users.noreply.github.com>
2023-08-10 00:52:31 +0530

    Update main.py
    adding device 'cpu' in place of 'cuba'

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ prompt = st.text_area("Enter a prompt:")
 
 # Load the model and set up the pipeline
 modelid = "CompVis/stable-diffusion-v1-4"
-device = "cuda"
-pipe = StableDiffusionPipeline.from_pretrained(modelid,use_auth_token=auth_token)
+device = "cpu"  # Change this to "cuda" if GPU is available, otherwise "cpu"
+pipe = StableDiffusionPipeline.from_pretrained(modelid, use_auth_token=auth_token)
 pipe.to(device)
 
 # Create a button to generate the image
@@ -33,4 +33,3 @@ if __name__ == '__main__':
     st.set_page_config(page_title="Stable Bud App", page_icon=":paintbrush:")
     st.set_option('deprecation.showfileUploaderEncoding', False)
     st.set_option('deprecation.showPyplotGlobalUse', False)
-
